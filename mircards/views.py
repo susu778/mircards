b8683015d19cbc1900f31a1ebc2dbe779b8ce34d
“Wlizzz” <15589055203@163.com>
2023-12-27 22:19:00 +0800

    框架 题目按钮

--- mircards/views.py
+++ mircards/views.py
@@ -6,3 +6,17 @@ from django.http import HttpResponse
 def index(request):
     test = "test!!!!!!"
     return render(request,"mircards/index.html", {"a": test})
+def home(request):
+    return render(request,"mircards/home.html")
+def search(request):
+    return render(request,"mircards/search.html")
+def mirset(request):
+    return render(request,"mircards/mirset.html")
+def analysis(request):
+    return render(request,"mircards/analysis.html")
+def download(request):
+    return render(request,"mircards/download.html")
+def help(request):
+    return render(request,"mircards/help.html")
+def about(request):
+    return render(request,"mircards/about.html")
