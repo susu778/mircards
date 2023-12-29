cfc0c891a386573595ffb222c9bb7e2a039b9e66
xueLLX <llx001126@163.com>
2023-12-29 13:36:03 +0800

    网站框架

--- mircards/urls.py
+++ mircards/urls.py
@@ -2,5 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index',views.index, name="index"),
+    path('index', views.index, name="index"),
+    path('search', views.search, name="search"),
+    path('mirset', views.mirset, name="mirset"),
+    path('analysis', views.analysis, name="analysis"),
+    path('download', views.download, name=" download"),
+    path('help', views.help, name="help"),
+    path('about', views.about, name="about"),
+
+
 ]
--- mircards/views.py
+++ mircards/views.py
@@ -6,3 +6,22 @@ from django.http import HttpResponse
 def index(request):
     test = "test!!!!!!"
     return render(request,"mircards/index.html",{"a": test})
+
+def search(request):
+    test = "test!!!!!!"
+    return render(request,"mircards/search.html",{"a": test})
+def mirset(request):
+    test = "test!!!!!!"
+    return render(request,"mircards/mirset.html",{"a": test})
+def analysis(request):
+    test = "test!!!!!!"
+    return render(request,"mircards/analysis.html",{"a": test})
+def download(request):
+    test = "test!!!!!!"
+    return render(request,"mircards/download.html",{"a": test})
+def help(request):
+    test = "test!!!!!!"
+    return render(request,"mircards/help.html",{"a": test})
+def about(request):
+    test = "test!!!!!!"
+    return render(request,"mircards/about.html",{"a": test})
